c635a8dd07b22fd87028b789373421f254ce0703
Phillip Wong <deepelegant99@gmail.com>
2022-01-30 22:57:20 -0800

    Added fond awesoem twitter icon

--- src/components/TwitterBox.js
+++ src/components/TwitterBox.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const TwitterBox = () => {
   return (
-    <a id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">
-      Twitter
-    </a>
+    // <a id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">
+    //   Twitter
+    // </a>
+    <i id="tweet-quote" className="fa fa-twitter fa_custom fa-4x"></i>
   );
 };
