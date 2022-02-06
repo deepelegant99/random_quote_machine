fd3289d05bb4e2394d023227788df41c3bb2cffc
Sahil <sahilg14@gmail.com>
2022-02-05 23:48:17 -0800

    A random quote appears on each page load

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -6,43 +6,31 @@ import { NewQuoteBox } from "./NewQuoteBox";
 import { TextBox } from "./TextBox";
 import { TwitterBox } from "./TwitterBox";
 
+const random = Math.floor(Math.random() * 1643) // Because api returns 1643 quotes with authors
+
 export const QuoteBox = () => {
+  const [quote, setQuote] = React.useState('')
+  const [colour, setColour] = React.useState('lightblue')
+  const [author, setAuthor] = React.useState('')
+  const api_url = 'https://type.fit/api/quotes'
 
-    const [quote, setQuote] = React.useState('');
-    const [colour, setColour] = React.useState('lightblue');
-    const [author, setAuthor] = React.useState('');
+  async function getapi(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data[random].text + '  -' + data[random].author)
+    setQuote(data[random].text)
+    setAuthor(data[random].author)
+  }
+  getapi(api_url)
 
-  const api_url ="https://type.fit/api/quotes";
-    
-  let data;
-  
-    async function getapi(url)
-    {
-      // url.preventDefault();
-
-      const response = await fetch(url);
-      data =await response.json();
-      console.log(data);
-      const random = Math.floor(Math.random() * 1643);
-      console.log(data[0].text + "  -"+data[0].author);
-
-      setQuote(data[0].text);
-      setAuthor(data[0].author);
-
-    }
-
-    getapi(api_url);
-
-  
-  // document.body.style.backgroundColor = colour
   document.getElementById('root').style.backgroundColor = colour
 
   return (
     <div id="quote-box" style={{ position: 'relative' }}>
-      <TextBox quote = {quote}/>
-      <AuthorBox author ={author}/>
+      <TextBox quote={quote} />
+      <AuthorBox author={author} />
       <NewQuoteBox setColour={setColour} />
       <TwitterBox />
     </div>
   )
-};
+}
