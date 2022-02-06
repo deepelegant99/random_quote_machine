230beacd58bd37562931a8d0a67f3cabf511db08
Sahil <sahilg14@gmail.com>
2022-02-06 00:03:43 -0800

    A random quote appears on each page load

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,27 +1,25 @@
-// Shortcut rafc for arrow function...
-
-import React from "react";
-import { AuthorBox } from "./AuthorBox";
-import { NewQuoteBox } from "./NewQuoteBox";
-import { TextBox } from "./TextBox";
-import { TwitterBox } from "./TwitterBox";
+import React from 'react'
+import { AuthorBox } from './AuthorBox'
+import { NewQuoteBox } from './NewQuoteBox'
+import { TextBox } from './TextBox'
+import { TwitterBox } from './TwitterBox'
 
 const random = Math.floor(Math.random() * 1643) // Because api returns 1643 quotes with authors
+const api_url = 'https://type.fit/api/quotes'
 
 export const QuoteBox = () => {
-  const [quote, setQuote] = React.useState('')
   const [colour, setColour] = React.useState('lightblue')
+  const [quote, setQuote] = React.useState('')
   const [author, setAuthor] = React.useState('')
-  const api_url = 'https://type.fit/api/quotes'
 
-  async function getapi(url) {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data[random].text + '  -' + data[random].author)
-    setQuote(data[random].text)
-    setAuthor(data[random].author)
-  }
-  getapi(api_url)
+  React.useEffect(() => {
+    fetch(api_url)
+      .then((results) => results.json())
+      .then((data) => {
+        setQuote(data[random].text)
+        setAuthor(data[random].author)
+      })
+  })
 
   document.getElementById('root').style.backgroundColor = colour
 
