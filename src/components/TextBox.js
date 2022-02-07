bc5c342de6c7845bbb61da86cd488b0a2a745c41
Phillip Wong <deepelegant99@gmail.com>
2022-02-06 21:53:19 -0800

    Added destructuring for the props passed into the components.

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 
-export const TextBox = (props) => {
+export const TextBox = ({quote}) => {
 
   return (
     <div id="text">
       <span style={{ marginRight: '10px' }}>
         <FontAwesomeIcon icon={faQuoteLeft} />
       </span>
-      {props.quote}
+      {quote}
       <br />
       <br />
     </div>
