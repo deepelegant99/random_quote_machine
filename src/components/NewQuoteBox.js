32ffcce68925f1db924994b9b80c21ecdf637dd0
Henry Crossley <crossh2001@yahoo.com>
2022-02-06 23:14:20 -0800

    random color

--- src/components/NewQuoteBox.js
+++ src/components/NewQuoteBox.js
@@ -3,7 +3,14 @@ import React from 'react';
 export const NewQuoteBox = ({ setColour, setRandom }) => {
   const handleSubmit = () => {
     let random = Math.floor(Math.random() * 1643);
-    setColour('lightgreen')
+    let randomColor = Math.floor(Math.random() * 5);
+    let newColor=Array(5);
+    newColor[0]="black";
+    newColor[1]="lightgreen";
+    newColor[2]="lightblue";
+    newColor[3]="red";
+    newColor[4]="orange";
+    setColour(newColor[randomColor])
     setRandom(random);
   }
   return (
